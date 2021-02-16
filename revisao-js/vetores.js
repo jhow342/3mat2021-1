684a56526434515b5d93c94814149e6104b0fdf3
jhow342 <jhonatanalvesvilar@outlook.com>
2021-02-16 21:42:07 +0000

    (15/02) Finalização da revisão de vetores

--- revisao-js/vetores.js
+++ revisao-js/vetores.js
@@ -19,7 +19,7 @@ console.log(primeiraFruta)
 // splice()
 // 1° parâmetro: posição que será removida (lembre-se que a contagem começa em zero)
 // 2° parâmetro: quantidade de elementos que serão removidos
-let terceiraFruta =fruta.splice(2, 1) // Fruta na posição 2
+let terceiraFruta = frutas.splice(2, 1) // Fruta na posição 2
 
 console.log(frutas)
 console.log(terceiraFruta) //splice() sempre retorna um vetor
@@ -52,3 +52,42 @@ console.log(frutas)
 frutas.splice(5, 1, 'abacate')
 console.log(frutas)
 
+/********************************************
+ * Percursos de vetores
+ */
+ // 1) percurso via for tradicinal
+ //é o mais flexivel, permitindo o percurso parcial (pode começar em um elemento que não seja o primeiro e terminar um elemento que não seja o ultimo)
+ for(let i = 0;  i < frutas.length; i++) {
+     console.log(i, frutas[i])
+ }
+ console.log('--------------------------------')
+
+ //2) Percurso via for tradicional, em ordem inversa
+ for(let i = frutas.length - 1; i >= 0; i--){
+     console.log(i,frutas[i])
+ }
+console.log('---------------------------------')
+ // 3) Percurso usando for..of
+ //percorre totalmente um vetor, sem nescessidade de manter uma variavel contadora
+ //variaveis:
+ //fruta -. nome da varivel que recebera cada elemento (pode ser qqr nome valido de variavel)
+ for(let fruta of frutas) {
+     console.log(fruta)
+ }
+
+ console.log('---------------------------------')
+ //4) percurso utilizado forEach()
+ // o nome do parametro pode ser qualquer coisa
+ frutas.forEach(function(elemento){
+     console.log(elemento)
+ })
+
+  console.log('---------------------------------')
+
+  //foreach() utilizado arrow function como parametro
+  frutas.forEach(elemento => console.log(elemento))
+  
+
+
+
+ 
